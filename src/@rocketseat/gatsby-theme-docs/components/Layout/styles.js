860fd4e256b1db3ef41b9a5b1d8864eee8bfd37a
Timothy Nguyen <nguyen.d.timothy@gmail.com>
2020-02-10 05:24:58 -0800

    resized middle panel

---
+++ src/@rocketseat/gatsby-theme-docs/components/Layout/styles.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.main`
+  margin-left: 280px;
+  padding: 2% 5% 0 5%;
+  max-width: 55rem;
+  height: 100%;
+
+  transition: transform 0.5s;
+
+  @media (max-width: 1000px) {
+    padding: 48px;
+  }
+
+  @media (max-width: 780px) {
+    padding: 24px 24px 48px 24px;
+    margin-left: 0;
+    transform: translate3d(
+      ${({ isMenuOpen }) => (isMenuOpen ? "240px" : "0")},
+      0,
+      0
+    );
+  }
+`;
